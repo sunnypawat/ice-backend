d2a2a4d96acb5080f58611c01593d78e09c5b559
FriendlyFire0725 <wichaya002@gmail.com>
2024-02-05 01:05:45 +0700

    add login

--- server/database.js
+++
@@ -1,8 +0,0 @@
-import mysql from mysql2
-
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'notes_app'
-}).promise()
--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE notes_app;
-USE notes_app
+CREATE DATABASE projectICE_db;
+USE projectICE_db
 
-CREATE TABLE article (
-    id integer PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    contents TEXT NOT NULL,
-    img VARCHAR(255),
-    created TIMESTAMP NOT NULL DEFAULT NOW()
-)
+CREATE TABLE Article (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    content TEXT,
+    imageLink VARCHAR(255)
+);
 
-INSERT INTO article(title, contents, img)
-VALUES 
-('First Aricle', 'Generic text woohoo ez trading 1'),
-('Second Aricle', 'Generic text woohoo ez trading 2')
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(255)
+);
